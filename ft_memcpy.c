c5cbfa1d773a2a1de932c77125282dc800689456
S4nxez <cobra_leina@hotmail.es>
2024-01-10 21:13:47 +0100

    ft_memcpy done without any tests

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansanc3 <dansanc3@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 20:08:27 by dansanc3          #+#    #+#             */
+/*   Updated: 2024/01/10 20:08:27 by dansanc3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+void	*memcpy(void *restrict	dest, const void *restrict	src, size_t	n)
+{
+	const unsigned char	*src_bytes;
+	unsigned char		*dest_bytes;
+	int					i;
+
+	dest_bytes = dest;
+	src_bytes = src;
+	i = 0;
+	while (i < n)
+	{
+		dest_bytes[i] = src_bytes[i];
+		i++;
+	}
+	return (dest);
+	//dale testeo en local aunq sea
+}
