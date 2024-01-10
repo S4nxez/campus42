7f869c232cf75ea8b07356473b10e15372bae496
S4nxez <cobra_leina@hotmail.es>
2024-01-10 19:33:21 +0100

    ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   normi.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dansanc3 <dansanc3@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dansanc3 <dansanc3@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 19:07:22 by dansanc3          #+#    #+#             */
-/*   Updated: 2024/01/09 19:38:53 by dansanc3         ###   ########.fr       */
+/*   Created: 2024/01/10 14:02:53 by dansanc3          #+#    #+#             */
+/*   Updated: 2024/01/10 14:02:53 by dansanc3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_memset(void *b, int c, int len)
+void	*ft_memset(void *b, int c, size_t len)
 {
- char *aux = b;
+	unsigned char	*aux;
 
- while (len > 0 && *aux != '\0')
- {
-  *aux = (char)c;
-  aux++;
-  len--;
- }
-}
-#include <stdio.h>
-int main()
-{
- char hola[] = "Hola";
- ft_memset(hola, 'c', 2);
- printf("memset: %s",hola);
+	aux = (unsigned char *)b;
+	while (len--)
+	{
+		*aux = (unsigned char)c;
+		aux++;
+	}
+	return (b);
 }
