2c8d67c2dcd05d96bba269ca57ec1238965a9c7e
S4nxez <cobra_leina@hotmail.es>
2024-01-24 18:18:21 +0100

    Add ft_atoi function and update ft_strnstr function

---
+++ ft_atoi.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansanc3 <dansanc3@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 16:16:25 by dansanc3          #+#    #+#             */
+/*   Updated: 2024/01/24 18:12:39 by dansanc3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	ret;
+	int	sign;
+
+	ret = 0;
+	i = 0;
+	while (ft_isspace(str[i]))
+	{
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		ret = ret * 10 + (str[i] - '0');
+		i++;
+	}
+	return (ret * sign);
+}
+
+int	ft_isspace(int c)
+{
+	if (c >= 9 && c <= 13 || c == ' ')
+		return (1);
+	return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Atoi og: %d\n", atoi("--12"));
+	printf("Atoi og: %d\n", atoi("-+12"));
+	printf("Atoi og: %d\n", atoi("++12"));
+	printf("Atoi og: %d\n", atoi(" -12323s2"));
+
+	printf("Atoi ft: %d\n", ft_atoi("--12"));
+	printf("Atoi ft: %d\n", ft_atoi("-+12"));
+	printf("Atoi ft: %d\n", ft_atoi("++12"));
+	printf("Atoi ft: %d\n", ft_atoi(" -12323s2"));
+}*/
