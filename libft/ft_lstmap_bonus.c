b50a79822a48d7226d3ad4495bba89852d6586ee
S4nxez <cobra_leina@hotmail.es>
2024-03-24 18:01:05 +0100

    added lstmap

---
+++ libft/ft_lstmap_bonus.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansanc3 <dansanc3@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 11:29:00 by dansanc3          #+#    #+#             */
+/*   Updated: 2024/03/24 11:29:00 by dansanc3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_obj;
+
+	if(!lst || !del || !f)
+		return (NULL);
+	while (lst->next)
+	{
+		new_obj = ft_lstnew(f(lst->content));
+		if(!new_obj)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_obj);
+		lst = lst->next;
+	}
+	return (new_list);
+}
