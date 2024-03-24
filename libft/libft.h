ce276bdc35f64d0f0a9f66c1488fbf1b8684bef3
S4nxez <cobra_leina@hotmail.es>
2024-03-24 18:01:55 +0100

    added lstmap and missing function

--- libft/libft.h
+++ libft/libft.h
@@ -13,8 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
-//# include <unistd.h>
-# include <io.h>
+# include <unistd.h>
 
 typedef struct s_list
 {
@@ -24,8 +23,8 @@ typedef struct s_list
 }					t_list;
 
 int		ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -59,10 +58,12 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
